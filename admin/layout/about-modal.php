781bf47cb18877e0c38f1833475508e4082e6a18
SAMSON <samsonjebaraj2022@gmail.com>
2024-05-19 19:25:21 +0530

    First Commit

---
+++ admin/layout/about-modal.php
@@ -0,0 +1,9 @@
+<div id="modal1" class="modal">
+    <div class="modal-content">
+      <h4>About MainRestrorent</h4>
+      <p>SAMSON is the owner </p>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Close</a>
+    </div>
+  </div>
---
+++ chunks/about.php
@@ -0,0 +1,8 @@
+<section class="fabout">
+		<div class="section white center">
+		      <div class="row container">
+		        <h2 class="header">ABOUT US</h2>
+		        <p class="grey-text text-darken-3 lighten-3">Autoscaling is a cloud computing technique for dynamically allocating computational resources. It involves automatically adjusting the number of servers or instances based on the load or demand. This allows for efficient use of resources, improved performance, and reduced costs. Autoscaling can be applied to various cloud platforms, including AWS, Azure, and Google Cloud.</p>
+				<a href="/RestroGirls/about-restro-girls.php" class="waves-effect waves-light btn" style="background: #ee6e73 !important;">Read More &raquo;</a>
+		      </div>
+	</section>
